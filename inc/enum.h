c0e8eea8b089b89d04a05f60b2108122120b9eda
Namkyung Koo <nakoo@c10r5s5.42seoul.kr>
2023-07-05 15:33:48 +0900

    Add : keycode

--- inc/enum.h
+++ inc/enum.h
@@ -6,7 +6,7 @@
 /*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 20:17:23 by nakoo             #+#    #+#             */
-/*   Updated: 2023/06/30 20:18:14 by nakoo            ###   ########.fr       */
+/*   Updated: 2023/07/05 15:19:33 by nakoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 
 # define X_EVENT_KEY_PRESS 2
 # define X_EVENT_KEY_EXIT 17
+
+enum e_keycode {
+	KEY_A = 0,
+	KEY_S = 1,
+	KEY_D = 2,
+	KEY_W = 13,
+	KEY_ESC = 53,
+	KEY_LEFT = 123,
+	KEY_RIGHT = 124,
+	KEY_DOWN = 125,
+	KEY_UP = 126
+};
 
 enum e_direction {
 	NORTH = 78,
--- src/render/main_loop.c
+++ src/render/main_loop.c
@@ -6,11 +6,41 @@
 /*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 17:07:45 by jisulee           #+#    #+#             */
-/*   Updated: 2023/06/30 21:17:01 by nakoo            ###   ########.fr       */
+/*   Updated: 2023/07/05 15:23:28 by nakoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	ft_close(t_data *data)
+{
+	mlx_destroy_image(data->mlx, data->screen_img.img);
+	mlx_destroy_window(data->mlx, data->win);
+	exit (0);
+}
+
+static void	key_press(int keycode, t_data *data)
+{
+	if (keycode == KEY_ESC)
+		ft_close(data);
+	else if (keycode == KEY_W)
+		;
+	else if (keycode == KEY_A)
+		;
+	else if (keycode == KEY_S)
+		;
+	else if (keycode == KEY_D)
+		;
+	else if (keycode == KEY_UP)
+		;
+	else if (keycode == KEY_DOWN)
+		;
+	else if (keycode == KEY_LEFT)
+		;
+	else if (keycode == KEY_RIGHT)
+		;
+	mlx_clear_window(data->mlx, data->win);
+}
 
 void	main_loop(t_data *data)
 {
@@ -21,6 +51,7 @@ void	main_loop(t_data *data)
 void	ft_mlx_loop(t_data *data)
 {
 	mlx_loop_hook(data->mlx, &main_loop, &data);
-	//mlx_hook(data->win, 2, 0, &key_press, &data);
+	mlx_hook(data->win, X_EVENT_KEY_PRESS, 0, key_press, data);
+	mlx_hook(data->win, X_EVENT_KEY_EXIT, 0, ft_close, data);
 	mlx_loop(data->mlx);
 }
